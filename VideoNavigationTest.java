e24bc1165411e31b835df847fc70e2902e434977
Gavin Peacock <gpeacock@adobe.com>
2016-10-04 20:01:04 -0700

    change name to videoNaviationTest

--- VideoNavigationTest.java
+++ VideoNavigationTest.java
@@ -86,7 +86,7 @@ import java.util.List;
 
 @Autonomous(name="Concept: Vuforia Navigation", group ="Concept")
 @Disabled
-public class ConceptVuforiaNavigation extends LinearOpMode {
+public class VideoNavigationTest extends LinearOpMode {
 
     public static final String TAG = "Vuforia Sample";
 
